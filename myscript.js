c097c4251ee82e702cd10d7d60e3bd20d3b507ff
Alitha Partono <alithapartono.a4jack@gmail.com>
2015-07-16 14:01:07 -0500

    deleted unnecessary things; changed some IDs

--- myscript.js
+++ myscript.js
@@ -1,25 +1,3 @@
 $(document).ready(function () {
-	$('#tabContent1').show();
-
-	for (j = 2; j < 8; j++) {
-		$('#tabContent' + j).hide();
-	}
-
-	$(".collapse").collapse();
-	$('#accordion').collapse({hide: true});
-});
-
-$('.nav a').click(function (e) {
-    $(this).tab('show');
-  
-  var tabContent = '#tabContent' + this.id; 
-  
-  $('#tabContent1').hide();
-  $('#tabContent2').hide();
-  $('#tabContent3').hide();
-  $(tabContent).show();
-});
-
-$('.panel a').click(function (e) {
-	$('#collapse' + location.hash).collapse('toggle');
+	location.hash && $(location.hash).collapse('show');
 });
