a27f884b77ff4d20a0e90edf5085d88c44004a7a
Alitha Partono <alithapartono.a4jack@gmail.com>
2015-07-21 10:49:09 -0500

    Linking to open accordions now work.

--- myscript.js
+++ myscript.js
@@ -1,3 +1,21 @@
 $(document).ready(function () {
-	location.hash && $(location.hash).collapse('show');
+	if (location.hash) {
+		goToLink(location.hash);
+	}
+
+	$('#anchorlink').click(function () {
+		goToLink(location.hash);
+	});
 });
+
+function goToLink(link) {
+	hashtag = link.replace("#", "").split("-");
+	if (hashtag.length > 1) {
+		$('.navbar a[href="#' + hashtag[0] + '"]').tab('show');
+		
+		if (hashtag.length > 2) {
+			$('#' + hashtag[0] + "-" + hashtag[1]).collapse('show');
+		}
+		$(location.hash).collapse('show');
+	}
+}
