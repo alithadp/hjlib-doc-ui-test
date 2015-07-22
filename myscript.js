9914d0368ecac51844a1e60aed75ff1d3ef06dc6
Alitha Partono <alithapartono.a4jack@gmail.com>
2015-07-22 12:10:13 -0500

    Added some comments to describe how each element of the documentation is formatted in the UI.

--- myscript.js
+++ myscript.js
@@ -11,8 +11,10 @@ $(document).ready(function () {
 function goToLink(link) {
 	hashtag = link.replace("#", "").split("-");
 
+	// Show tab
 	$('.navbar a[href="#' + hashtag[0] + '"]').tab('show');
 
+	// Show accordion if link refers to an accordion
 	if (hashtag.length > 1) {
 		if (hashtag.length > 2) {
 			$('#' + hashtag[0] + "-" + hashtag[1]).collapse('show');
