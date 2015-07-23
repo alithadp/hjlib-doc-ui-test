f1b228c94ea446a807d9c79b3d94620c2ceb3e6e
Alitha Partono <alithapartono.a4jack@gmail.com>
2015-07-23 10:42:05 -0500

    Fixed anchor links.

--- myscript.js
+++ myscript.js
@@ -3,22 +3,24 @@ $(document).ready(function () {
 		goToLink(location.hash);
 	}
 
-	$('#anchorlink').click(function () {
-		goToLink(location.hash);
+	
+	$('.anchorlink').click(function () {
+		goToLink(this.getAttribute("href"));
 	});
 });
 
 function goToLink(link) {
 	hashtag = link.replace("#", "").split("-");
+	alert(hashtag);
 
 	// Show tab
 	$('.navbar a[href="#' + hashtag[0] + '"]').tab('show');
 
 	// Show accordion if link refers to an accordion
 	if (hashtag.length > 1) {
+		$('#' + hashtag[0] + "-" + hashtag[1]).collapse('show');
 		if (hashtag.length > 2) {
-			$('#' + hashtag[0] + "-" + hashtag[1]).collapse('show');
+			$('#' + hashtag[0] + "-" + hashtag[1] + "-" + hashtag[2]).collapse('show');
 		}
-		$(location.hash).collapse('show');
 	}
 }
